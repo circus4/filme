34e4122d3fd9430427fa30afa13fe9f246026f25
kayo289 <kayokapi@icloud.com>
2019-05-12 19:27:06 +0900

    [add]profile編集ボタン

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -5,4 +5,17 @@ class User::UsersController < ApplicationController
     def edit
       @user = current_user
     end
+
+    def update
+      user=User.find(params[:id])
+      user.update(user_params)
+      redirect_to edit_user_path(user.id)
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:image,:phonetic_name,:name,:email,:postal_code,:prefecture,
+                                   :city,:adress,:phone_number)
+    end
 end
